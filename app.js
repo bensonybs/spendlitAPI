b503c4908cf0cc74f47d6d18f5ed332214a19f1b
Benson <yehbo0921@gmail.com>
2023-03-22 21:49:08 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(port, () => console.log(`Spendlit API listening on port http://localhost:${port}`))
