25ffad6984e03f169827d02ad3083031e16b8f89
Benson <yehbo0921@gmail.com>
2023-03-30 18:20:18 +0800

    feat: User authenticate, register passport.js

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const routes = require('./routes')
+const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use('api/v1', routes)
+app.use(express.json())
+app.use(passport.initialize())
+
+app.use('/api/v1', routes)
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
