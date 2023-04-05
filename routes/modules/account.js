6f8fdec8193911d7048f011817a4df8aa5fd5df2
Ting-Yu, Lin <tingyulin68@gmail.com>
2023-04-06 00:04:19 +0800

    feat: add putAccounts

--- routes/modules/account.js
+++ routes/modules/account.js
@@ -2,14 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 const accountController = require('../../controllers/account-controller')
-const { addAccountValidator } = require('../../middleware/input-validation')
+const { accountValidator } = require('../../middleware/input-validation')
 
 router.route('/types').get(accountController.getAccountTypes) // A_02
-router.route('/:id').delete(accountController.deleteAccount) // A_05
+router
+  .route('/:id')
+  .put(accountValidator, accountController.putAccount) // A_04
+  .delete(accountController.deleteAccount) // A_05
 router
   .route('/')
   .get(accountController.getAccounts) // A_01
-  .post(addAccountValidator, accountController.postAccounts) // A_03
-
+  .post(accountValidator, accountController.postAccounts) // A_03
 
 module.exports = router
