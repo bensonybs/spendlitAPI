17fc2d2221df748997c899a4f496d7a077451e49
Benson <yehbo0921@gmail.com>
2023-08-15 11:41:02 +0800

    feat: Add swagger

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+const fs = require('fs')
+const YAML = require('yaml')
+
 const routes = require('./routes')
 const passport = require('./config/passport')
+const swaggerFile = fs.readFileSync('./swagger.yaml', 'utf-8')
+const swaggerDocument = YAML.parse(swaggerFile)
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(passport.initialize())
-
+// APIs routers
 app.use('/api/v1', routes)
+// APIs Document
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+// Test route for the service
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
